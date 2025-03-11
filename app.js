22b6ef02551617dbb6d5b867cb593e769a076b87
Bat-sheva Birenbaum <b0556772946@gmail.com>
2025-03-11 16:54:03 +0200

    add func get

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import express from 'express'
 
+const app = express()
+const port = 5000;
 const options = {
     method: 'GET',
     url: 'https://api.render.com/v1/services?includePreviews=true&limit=20',
@@ -13,3 +16,17 @@ const options = {
     .request(options)
     .then(res => console.log(res.data))
     .catch(err => console.error(err));
+
+    app.get('/',(req,res)=>{
+        axios
+             .request(options)
+             .then(response => {
+                res.json(response.data);
+             })
+             .catch(error=>{
+                res.status(500).json({error:"Error is running data from Render APi"})
+             });
+    })
+    app.listen(port,()=>{
+        console.log(`server is running on http://localhost:${port}`);
+    })
